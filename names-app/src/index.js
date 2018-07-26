7247441559ebfcb9686e3f80200150b71b0f2767
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2018-07-25 21:29:53 -0300

    Update
    Add Amplify configuration

--- names-app/src/index.js
+++ names-app/src/index.js
@@ -4,6 +4,20 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
+import Amplify from "aws-amplify";
+import config from "./config";
+
+Amplify.configure({
+    API: {
+        endpoints: [
+            {
+                name: "names",
+                endpoint: config.apiGateway.URL,
+                region: config.apiGateway.REGION
+            },
+        ]
+    }
+});
 
 ReactDOM.render(
   <Router>
