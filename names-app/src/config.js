8bb6b67ad78f3d934b928b4739693d9545bedfa7
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2018-07-25 21:24:54 -0300

    Add file

---
+++ names-app/src/config.js
@@ -0,0 +1,8 @@
+export default {
+    s3: {
+        apiGateway: {
+            REGION: "us-east-1",
+            URL: "https://0cio16i5cb.execute-api.us-east-1.amazonaws.com/prod"
+        }
+    }
+};
